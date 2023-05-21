b5a274c3f0b6e6a9bd9ee43a723a490d6344d21a
Ryan <jokytalki3@gmail.com>
2023-05-21 20:11:42 +0800

    use timeout

--- uploadClippings.js
+++ uploadClippings.js
@@ -17,22 +17,23 @@ fs.readFile(filePath, async (err, inputD) => {
     let clippingArray = inputD.toString().split('==========')
     clippingArray.pop()
     for (let clipping of clippingArray) {
-        let [title, timeLocation, highlight] = clipping.split(/\r?\n/).filter(item => item)
-        let exist = await findPageByTitle(title, allPages)
-        if (exist) { // proceed to insert more highlights
-            let pageId = exist.id
-            appendHighlights(pageId, highlight)
-            continue
-        }
-        // proceed to create
-        insertPage(title, highlight)
+        setTimeout(async () => {
+            let [title, timeLocation, highlight] = clipping.split(/\r?\n/).filter(item => item)
+            let exist = await findPageByTitle(title, allPages)
+            if (exist) { // proceed to insert more highlights
+                let pageId = exist.id
+                appendHighlights(pageId, highlight)
+            } else {
+                insertPage(title, highlight)
+            }
+        }, 2000)
     }
 })
 
 
 function findPageByTitle(title, allPages) {
     for (let page of allPages) {
-        if (page.properties?.Name?.title[0]?.text.content === title) {
+        if (page.properties?.Name?.title[0]?.plain_text === title) {
             return page
         }
     }
